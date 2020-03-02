e9ebc62230b1861ae759c2299a3f63643d9c93a9
damiloyola <damiloyola@gmail.com>
2020-03-02 20:52:18 -0300

    se asignan variables de medidas y color

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sorteador from './components/Sorteador';
+import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+
 
 ReactDOM.render(<Sorteador/>, document.getElementById('app'));
